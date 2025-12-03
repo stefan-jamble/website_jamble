62f30a6607732b5e1daf7b69b1dcd47f26a1d0dd
Stefan <stefan@jamble.com>
2025-12-03 17:28:49 +0100

    add the smart app banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-itunes-app" content="app-id=1599696300" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
