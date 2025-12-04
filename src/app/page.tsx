9e8152ca7c452831b5ef7e7810c4f4df69f2f589
Stefan <stefan@jamble.com>
2025-12-04 14:16:48 +0100

    app banner workiiiiiing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,6 @@ export default async function HomePage() {
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <SmartBanner
-        appName="Jamble"
-        appStoreUrl="https://apps.apple.com/app/id123456789"
-        playStoreUrl="https://play.google.com/store/apps/details?id=com.example.app"
-        delay={500}
-      />
-
       <iframe
         src={iframeSrc}
         style={{ width: "100%", height: "100%", border: "none" }}
