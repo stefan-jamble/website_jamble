803f4becaaaafabd22c2f96c702994a52de620f7
Stefan <stefan@jamble.com>
2025-12-05 11:07:38 +0100

    dub analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics as DubAnalytics } from "@dub/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <DubAnalytics />
     </html>
   );
 }
