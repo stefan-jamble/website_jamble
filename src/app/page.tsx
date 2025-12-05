bdc84f80b2bd93c4de447a49d39643b055d4b50a
Stefan <stefan@jamble.com>
2025-12-05 09:11:44 +0100

    remove button above iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,6 @@ export default async function HomePage() {
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <a
-        href="https://lestudioslingshot.fr/"
-        className="w-full h-12 rounded-full text-xl bg-[#7E53F8] text-white flex justify-center items-center"
-      >
-        "alreadyClaimedButton"
-      </a>
       <iframe
         src={iframeSrc}
         style={{ width: "100%", height: "100%", border: "none" }}
