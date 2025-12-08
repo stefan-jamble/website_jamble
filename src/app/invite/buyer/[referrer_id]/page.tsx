0301856e815206eafd19273f0e896dd49bc8b3fe
Stefan <stefan@jamble.com>
2025-12-08 17:23:48 +0100

    fixed

--- src/app/invite/buyer/[referrer_id]/page.tsx
+++ src/app/invite/buyer/[referrer_id]/page.tsx
@@ -11,6 +11,8 @@ export default async function InviteWithReferrerPage({ params }: PageProps) {
   const { referrer_id } = await params;
   const countryCode = await getCountryCode();
 
+  console.log("countryCode: ", countryCode)
+
   return (
     <BuyerInvite countryCode={countryCode} referrerId={referrer_id} />
   );
