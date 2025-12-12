41073b765120ca1b329fdfc04691b0a09f8092eb
Stefan <stefan@jamble.com>
2025-12-12 14:44:49 +0100

    changed wow

---
+++ src/app/invite/seller/[partner_username]/page.tsx
@@ -0,0 +1,66 @@
+import AffiliateInvite from "./AffiliateInvite";
+import { getCountryCode } from "@/lib/location";
+import { cookies } from "next/headers";
+
+interface PageProps {
+    params: {
+        partner_username: string;
+    };
+    searchParams?: {
+        [key: string]: string | string[] | undefined;
+    };
+}
+
+interface PartnerProfileData {
+    id: string;
+    username: string;
+}
+
+async function fetchPartnerProfile(partner_username: string) {
+    const headers = {
+        Authorization: "Bearer ca9d3151-ebdd-4902-8a61-63a2564838ce",
+        "Content-Type": "application/json"
+    }
+
+    const res = await fetch(
+        "https://jamble-backend-test-us-576189464787.us-central1.run.app/profile/get_profile_from_username",
+        {
+            method: "POST",
+            headers,
+            body: JSON.stringify({ username: partner_username }),
+            cache: "no-store",
+        }
+    );
+
+    const data = await res.json();
+
+    if (!data.success) {
+        throw new Error("Failed to fetch partner profile");
+    }
+
+    return data.payload.profile as PartnerProfileData;
+
+}
+
+
+export default async function InviteWithPartnerPage({ params, searchParams }: PageProps) {
+    const { partner_username } = await params;
+    const countryCode = await getCountryCode();
+    const resolvedSearchParams = await searchParams;
+    const utmCampaign = (resolvedSearchParams?.utm_campaign ?? undefined) as string | undefined;
+
+    const cookieStore = await cookies();
+    const dubId = cookieStore.get("dub_id")?.value;
+
+    const partner = await fetchPartnerProfile(partner_username)
+
+
+    return (
+        <AffiliateInvite 
+            countryCode={countryCode} 
+            partnerId={partner.id} 
+            utmCampaign={utmCampaign}
+            dubId={dubId}
+        />
+    );
+}
