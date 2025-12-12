c4db7a907bde8aaebf0d360586c7cdb78aaa88db
Stefan <stefan@jamble.com>
2025-12-12 10:01:10 +0100

    dynamic inception link fixed

--- src/app/[username]/route.ts
+++ src/app/[username]/route.ts
@@ -1,10 +1,10 @@
 import { redirect } from "next/navigation";
 
-export function GET(
+export async function GET(
     _request: Request,
     { params }: { params: { username: string }}
 ) {
-    const { username } = params;
+    const { username } = await params;
     const dubLink = `https://br.jamble.com/${username}`
     return redirect(dubLink);
 }
